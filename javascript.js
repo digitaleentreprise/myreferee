eee36efb9173f88af4e0d41505f0e3349fc98fb2
denis-bertin <60503493+denis-bertin@users.noreply.github.com>
2026-01-15 07:04:19 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,122 @@
+function TaggingText(ButtonId) {
+    if ( ButtonId == "None" ) {
+        Elements = document.getElementsByClassName("Niveau1");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.remove("YellowText");
+        }
+        Elements = document.getElementsByClassName("Niveau2");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.remove("GreenText");
+        }
+        Elements = document.getElementsByClassName("Niveau3");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.remove("BlueText");
+        }
+        Viewers = document.getElementsByClassName("Viewer");
+        for (var i = Viewers.length - 1; i >= 0; i--) {
+            Viewers[i].className = "Viewer GreyText";
+        }
+    } else {
+        if ( ButtonId == "Niveau1" || ButtonId == "All") {
+            Elements = document.getElementsByClassName("Niveau1");
+            for (var i = Elements.length - 1; i >= 0; i--) {
+                Elements[i].classList.add("YellowText");
+            }
+            Viewer = document.getElementById("ViewNiveau1");
+            Viewer.classList.remove("GreyText");
+            Viewer.classList.add("YellowText");
+        }
+        if ( ButtonId == "Niveau2" || ButtonId == "All") {
+            Elements = document.getElementsByClassName("Niveau2");
+            for (var i = Elements.length - 1; i >= 0; i--) {
+                Elements[i].classList.add("GreenText");
+            }
+            Viewer = document.getElementById("ViewNiveau2");
+            Viewer.classList.remove("GreyText");
+            Viewer.classList.add("GreenText");
+        }
+        if ( ButtonId == "Niveau3" || ButtonId == "All") {
+            Elements = document.getElementsByClassName("Niveau3");
+            for (var i = Elements.length - 1; i >= 0; i--) {
+                Elements[i].classList.add("BlueText");
+            }
+            Viewer = document.getElementById("ViewNiveau3");
+            Viewer.classList.remove("GreyText");
+            Viewer.classList.add("BlueText");
+        }
+    }
+}
+function DisplayElements(ButtonId) {
+    if ( ButtonId == "Title" ) {
+        Elements = document.getElementsByTagName("p");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            if ( Elements[i].className != "PreAmbule" ) {
+                Elements[i].classList.add("UnDisplay");
+            }
+        }
+        Elements = document.getElementsByTagName("h2");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.add("UnDisplay");
+        }
+        Elements = document.getElementsByTagName("ul");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.add("UnDisplay");
+        }
+        Elements = document.getElementsByTagName("table");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.add("UnDisplay");
+        }
+    } else if ( ButtonId == "All" ) {
+        Elements = document.getElementsByTagName("p");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            if ( Elements[i].className != "PreAmbule" ) {
+                Elements[i].classList.remove("UnDisplay");
+            }
+        }
+        Elements = document.getElementsByTagName("h2");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.remove("UnDisplay");
+        }
+        Elements = document.getElementsByTagName("ul");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.remove("UnDisplay");
+        }
+        Elements = document.getElementsByTagName("table");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            Elements[i].classList.remove("UnDisplay");
+        }
+    } else if ( ButtonId == "Tagged" ) {
+        Elements = document.getElementsByTagName("p");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            if ( Elements[i].className.includes("Text") ) {
+                Elements[i].classList.remove("UnDisplay");
+            } else {
+                Elements[i].classList.add("UnDisplay");
+            }
+        }
+        Elements = document.getElementsByTagName("h2");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            if ( Elements[i].className.includes("Text") ) {
+                Elements[i].classList.remove("UnDisplay");
+            } else {
+                Elements[i].classList.add("UnDisplay");
+            }
+        }
+        Elements = document.getElementsByTagName("ul");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            if ( Elements[i].className.includes("Text") ) {
+                Elements[i].classList.remove("UnDisplay");
+            } else {
+                Elements[i].classList.add("UnDisplay");
+            }
+        }
+        Elements = document.getElementsByTagName("table");
+        for (var i = Elements.length - 1; i >= 0; i--) {
+            if ( Elements[i].className.includes("Text") ) {
+                Elements[i].classList.remove("UnDisplay");
+            } else {
+                Elements[i].classList.add("UnDisplay");
+            }
+        }
+    }
+}
